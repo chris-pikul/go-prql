2a0359f12edf0254b9bd177cc9e7bcd64b83009d
Chris Pikul <chris@workmaple.com>
2022-06-29 16:43:52 -0700

    fix for comment

--- prql.go
+++ prql.go
@@ -3,7 +3,7 @@ package prql
 // Compile takes an incoming PRQL query (string) and returns the SQL standard
 // equivelent (string), or an error if one occured. In the event of an error,
 // the string returned will be empty. The error type is a custom type wrapping
-// the go standard error as PRQLError.
-func Compile(source string) (string, *PRQLError) {
+// the go standard error as "prql.Error".
+func Compile(source string) (string, *Error) {
 	return "", nil
 }
