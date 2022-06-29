84db9b0075cdd27bb67c389d13e590cc838f11f2
Chris Pikul <chris@workmaple.com>
2022-06-29 16:43:26 -0700

    added syntax type for Dialect. added syntax type for Query. added Query to PRSQL string.

---
+++ syntax/dialect.go
@@ -0,0 +1,61 @@
+package syntax
+
+import (
+	"fmt"
+
+	"github.com/chris-pikul/go-prql/utils"
+)
+
+// Dialect is a byte enum representing the accepted dialects. This is declared
+// by the top-level "prsql" expression.
+type Dialect byte
+
+const (
+	DialectGeneric Dialect = iota
+	DialectANSI
+	DialectBigQuery
+	DialectClickHouse
+	DialectHive
+	DialectMSSQL
+	DialectMYSQL
+	DialectPostgres
+	DialectSQLite
+	DialectSnowflake
+)
+
+var dialectStringMap = map[Dialect]string{
+	DialectGeneric:    "generic",
+	DialectANSI:       "ansi",
+	DialectBigQuery:   "bigquery",
+	DialectClickHouse: "clickhouse",
+	DialectHive:       "hive",
+	DialectMSSQL:      "mssql",
+	DialectMYSQL:      "mysql",
+	DialectPostgres:   "postgres",
+	DialectSQLite:     "sqlite",
+	DialectSnowflake:  "snowflake",
+}
+
+// String returns the string representation of the underlying Dialect enum. If
+// invalid, defaults to returning "generic".
+func (d Dialect) String() string {
+	str, ok := dialectStringMap[d]
+	if ok {
+		return str
+	}
+	return "generic"
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface. Allows for
+// reading strings into their Dialect enum value. Returns an error if the text
+// is invalid and does not match a known dialect.
+//
+// Important: this is CASE-SENSITIVE.
+func (d *Dialect) UnmarshalText(text []byte) error {
+	str := string(text)
+	if key, ok := utils.KeyForValue(dialectStringMap, str); ok {
+		*d = *key
+		return nil
+	}
+	return fmt.Errorf("text '%s' is not a valid Dialect", str)
+}
---
+++ syntax/query.go
@@ -0,0 +1,32 @@
+package syntax
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/chris-pikul/go-prql/utils"
+)
+
+// Query represents the top-level "prsql" dialect and version declaration
+type Query struct {
+	Version utils.Optional[int]
+	Dialect Dialect
+}
+
+// String returns the PRSQL expression for defining this Query.
+func (q Query) String() string {
+	var str strings.Builder
+	str.WriteString("prsql")
+
+	if ver, ok := q.Version.Get(); ok {
+		str.WriteString(" version:")
+		str.WriteString(strconv.Itoa(*ver))
+	}
+
+	if q.Dialect != DialectGeneric {
+		str.WriteString(" dialect:")
+		str.WriteString(q.Dialect.String())
+	}
+
+	return str.String()
+}
