71455e93120662c7cee6028d9cd3f2bd22ff34cb
Chris Pikul <chris@workmaple.com>
2022-06-29 17:52:47 -0700

    added util to invert maps

--- utils/map.go
+++ utils/map.go
@@ -13,3 +13,15 @@ func KeyForValue[K comparable, V comparable](tgt map[K]V, value V) (*K, bool) {
 
 	return nil, false
 }
+
+// InvertMap returns a new map with all the given key-values on the target map,
+// swapped. That is, the keys become values, and the values become the keys.
+//
+// Both the key and value must be comparable.
+func InvertMap[K comparable, V comparable](tgt map[K]V) map[V]K {
+	result := make(map[V]K, len(tgt))
+	for key, val := range tgt {
+		result[val] = key
+	}
+	return result
+}
